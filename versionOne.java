86bedf596afee25f057ee1e4d6666808f5fe75c0
erik-perez <68122693+erik-perez@users.noreply.github.com>
2021-12-28 09:48:05 -0500

    Update versionOne.java

--- versionOne.java
+++ versionOne.java
@@ -2,18 +2,18 @@ import java.io.PrintStream;
 
 public class versionOne {
     static void revealStr(char binaryString[], int currentIndex, PrintStream write) {
-        if(currentIndex == binaryString.length) { //Checks if the current index is equals to length of the character array
-            write.println(binaryString); //prints it to the file
+        if(currentIndex == binaryString.length) { 
+            write.println(binaryString); 
             return;
         }
-        if(binaryString[currentIndex] == '*') { // looks through to see if there's a * in the string
-            binaryString[currentIndex] = '0'; //replace the current index with a 0
-            revealStr(binaryString, currentIndex + 1, write);  //calls the method and increases index by 1
-            binaryString[currentIndex] = '1'; 	   // replaces the current index with 1
-            revealStr(binaryString, currentIndex + 1, write); //calls the method and increases index by 1
-            binaryString[currentIndex] = '*';   //replaces the current index to *
+        if(binaryString[currentIndex] == '*') { 
+            binaryString[currentIndex] = '0'; 
+            revealStr(binaryString, currentIndex + 1, write);  
+            binaryString[currentIndex] = '1'; 	   
+            revealStr(binaryString, currentIndex + 1, write); 
+            binaryString[currentIndex] = '*';   
         }
         else
-            revealStr(binaryString, currentIndex + 1, write); //calls the method and increases index by 1
+            revealStr(binaryString, currentIndex + 1, write); 
     }
 }
