cf91e56edb7bc442582f6705c60e6da06b59a48f
erik-perez <68122693+erik-perez@users.noreply.github.com>
2021-12-28 09:47:25 -0500

    Update GenerateNumber.java

--- GenerateNumber.java
+++ GenerateNumber.java
@@ -1,11 +1,11 @@
 public class GenerateNumber {
     public static String genString(int n) {
-        String options = "10*"; //provides the different chars that the string can be made of
-        StringBuilder sb = new StringBuilder(n); //allows for the construction of a string
-        for (int i = 0; i<n; i++) { //populates each position of the string with either a 1, 0 or *, runs until n which is determined in the driver
+        String options = "10*";
+        StringBuilder sb = new StringBuilder(n); 
+        for (int i = 0; i<n; i++) { 
             int index = (int)(options.length()*Math.random());
             sb.append(options.charAt(index));
         }
-        return sb.toString(); //returns the now generated string
+        return sb.toString(); 
     }
 }
