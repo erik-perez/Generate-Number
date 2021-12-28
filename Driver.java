7e3a04619e62d816808f55860c463b8ad7fec775
erik-perez <68122693+erik-perez@users.noreply.github.com>
2021-12-28 09:46:43 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,26 +1,26 @@
 import java.io.*;
 
 public class Driver {
-    static int n = 20;	//gives the length of the string to be generated
+    static int n = 20;
     public static void main(String[] args) {
-        String Tester = GenerateNumber.genString(n); // Creates a string to test
-        char[] binaryString = Tester.toCharArray(); // Converts the string to char array
+        String Tester = GenerateNumber.genString(n); 
+        char[] binaryString = Tester.toCharArray(); 
         PrintStream printWrite = null;
         try {
-            File fileWrite = new File("out.txt"); //name of the file being written to
+            File fileWrite = new File("out.txt"); 
             printWrite = new PrintStream(new BufferedOutputStream(new FileOutputStream(fileWrite, true)));
-            printWrite.println("\nRecursive Test: " + Tester); //prints the first recursive test and the string being fixed
-            long v1StartTime = System.currentTimeMillis(); //sets start time
-            versionOne.revealStr(binaryString, 0, printWrite);   //calls the method in order to show each possible outcome and to print to the file
-            long v1EndTime = System.currentTimeMillis(); //sets end time
-            long v1TimeElapsed = v1EndTime - v1StartTime; //calculates time elapsed
-            printWrite.print(v1TimeElapsed + " Milliseconds"); //prints the amount of time needed to operate
-            printWrite.println("\nIterative Test: " + Tester);   //prints the first iterative test and the string being fixed
-            long v2StartTime = System.currentTimeMillis(); //sets start time
-            versionTwo.revealStrIterative(Tester, printWrite);  //calls the method in order to show each possible outcome and to print to the file
-            long v2EndTime = System.currentTimeMillis(); //sets end time
-            long v2TimeElapsed = v2EndTime - v2StartTime; //calculates time elapsed
-            printWrite.print(v2TimeElapsed + " Milliseconds"); //prints the amount of time needed to operate
+            printWrite.println("\nRecursive Test: " + Tester); 
+            long v1StartTime = System.currentTimeMillis(); 
+            versionOne.revealStr(binaryString, 0, printWrite);   
+            long v1EndTime = System.currentTimeMillis(); 
+            long v1TimeElapsed = v1EndTime - v1StartTime; 
+            printWrite.print(v1TimeElapsed + " Milliseconds"); 
+            printWrite.println("\nIterative Test: " + Tester);   
+            long v2StartTime = System.currentTimeMillis(); 
+            versionTwo.revealStrIterative(Tester, printWrite);  
+            long v2EndTime = System.currentTimeMillis(); 
+            long v2TimeElapsed = v2EndTime - v2StartTime; 
+            printWrite.print(v2TimeElapsed + " Milliseconds"); 
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
